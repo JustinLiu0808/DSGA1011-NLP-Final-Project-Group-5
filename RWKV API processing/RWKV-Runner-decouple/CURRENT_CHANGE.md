050120a0d393f5757df5a6e43b621cafd4cdc7dd
JustinLiu0808 <jl12060@nyu.edu>
2025-12-09 21:59:43 -0500

    Add files via upload

---
+++ RWKV API processing/RWKV-Runner-decouple/CURRENT_CHANGE.md
@@ -0,0 +1,22 @@
+## Changes
+
+- add extra vc++ installation guide
+- add support for REMI tokenizer and raw token input
+- allow prompt to be empty when using `/completions` api
+- bump precompiled llama.cpp vulkan
+- add modelName and date to screenshot
+- update python dependencies
+- update manifest.json and defaultConfigs
+- chores
+
+Note: If you encounter WebView2 crash issues, please try opening the Windows Settings, click on Apps, search for
+WebView2, click Modify -> Repair to update your WebView2 runtime.
+
+## Install
+
+- Windows: https://github.com/josStorer/RWKV-Runner/blob/master/build/windows/Readme_Install.txt
+- MacOS: https://github.com/josStorer/RWKV-Runner/blob/master/build/darwin/Readme_Install.txt
+- Linux: https://github.com/josStorer/RWKV-Runner/blob/master/build/linux/Readme_Install.txt
+- Simple Deploy Example: https://github.com/josStorer/RWKV-Runner/blob/master/README.md#simple-deploy-example
+- Server Deploy Examples: https://github.com/josStorer/RWKV-Runner/tree/master/deploy-examples
+- Windows 7 Patches: https://github.com/josStorer/wails/releases/tag/v2.9.2x
---
+++ RWKV API processing/RWKV-Runner-decouple/main.go
@@ -0,0 +1,143 @@
+package main
+
+import (
+	"embed"
+	"fmt"
+	"net/http"
+	"os"
+	"strings"
+
+	backend "rwkv-runner/backend-golang"
+
+	"github.com/wailsapp/wails/v2"
+	wailsLogger "github.com/wailsapp/wails/v2/pkg/logger"
+	"github.com/wailsapp/wails/v2/pkg/options"
+	"github.com/wailsapp/wails/v2/pkg/options/assetserver"
+	"github.com/wailsapp/wails/v2/pkg/options/windows"
+)
+
+type FileLoader struct {
+	http.Handler
+}
+
+func NewFileLoader() *FileLoader {
+	return &FileLoader{}
+}
+
+func (h *FileLoader) ServeHTTP(res http.ResponseWriter, req *http.Request) {
+	var err error
+	requestedFilename := strings.TrimPrefix(req.URL.Path, "/")
+	requestedFilename = strings.TrimPrefix(requestedFilename, "=>") // absolute path
+	println("Requesting file:", requestedFilename)
+	fileData, err := os.ReadFile(requestedFilename)
+	if err != nil {
+		res.WriteHeader(http.StatusBadRequest)
+		res.Write([]byte(fmt.Sprintf("Could not load file %s", requestedFilename)))
+	}
+
+	res.Write(fileData)
+}
+
+//go:embed all:frontend/dist
+var assets embed.FS
+
+//go:embed all:py310/Lib/site-packages/cyac
+var cyac embed.FS
+
+//go:embed all:py310/Lib/site-packages/cyac-1.9.dist-info
+var cyacInfo embed.FS
+
+//go:embed all:py310/Lib/site-packages/llama_cpp
+var llamacpp embed.FS
+
+//go:embed all:py310/Lib/site-packages/llama_cpp_python-0.3.16.dist-info
+var llamacppInfo embed.FS
+
+//go:embed backend-python
+var py embed.FS
+
+// //go:embed backend-rust
+// var webgpu embed.FS
+
+//go:embed all:finetune
+var finetune embed.FS
+
+//go:embed midi
+var midi embed.FS
+
+//go:embed assets/sound-font
+var midiAssets embed.FS
+
+//go:embed components
+var components embed.FS
+
+func main() {
+	// Create an instance of the app structure
+	app := backend.NewApp()
+	app.Dev = true
+
+	if !backend.IsDebugMode() {
+		app.Dev = false
+
+		backend.CopyEmbed(assets)
+		os.RemoveAll("./py310/Lib/site-packages/cyac-1.7.dist-info")
+		os.RemoveAll("./py310/Lib/site-packages/llama_cpp_python-0.3.9.dist-info")
+		backend.CopyEmbed(cyac)
+		backend.CopyEmbed(cyacInfo)
+		backend.CopyEmbed(llamacpp)
+		backend.CopyEmbed(llamacppInfo)
+		backend.CopyEmbed(py)
+		// backend.CopyEmbed(webgpu)
+		backend.CopyEmbed(finetune)
+		backend.CopyEmbed(midi)
+		backend.CopyEmbed(midiAssets)
+		backend.CopyEmbed(components)
+	}
+
+	var zoomFactor float64 = 1.0
+	data, err := app.ReadJson("config.json")
+	if err == nil {
+		app.HasConfigData = true
+		app.ConfigData = data.(map[string]any)
+		if dpiScaling, ok := app.ConfigData["settings"].(map[string]any)["dpiScaling"]; ok {
+			zoomFactor = dpiScaling.(float64) / 100
+		}
+	} else {
+		app.HasConfigData = false
+	}
+
+	var logger wailsLogger.Logger
+	if app.Dev {
+		logger = wailsLogger.NewDefaultLogger()
+	} else {
+		logger = wailsLogger.NewFileLogger("crash.log")
+	}
+
+	// Create application with options
+	err = wails.Run(&options.App{
+		Title:                    "RWKV-Runner",
+		Width:                    1024,
+		Height:                   700,
+		MinWidth:                 375,
+		MinHeight:                640,
+		EnableDefaultContextMenu: true,
+		Windows: &windows.Options{
+			ZoomFactor:           zoomFactor,
+			IsZoomControlEnabled: true,
+		},
+		AssetServer: &assetserver.Options{
+			Assets:  assets,
+			Handler: NewFileLoader(),
+		},
+		OnStartup:     app.OnStartup,
+		OnBeforeClose: app.OnBeforeClose,
+		Bind: []any{
+			app,
+		},
+		Logger: logger,
+	})
+
+	if err != nil {
+		println("Error:", err.Error())
+	}
+}
